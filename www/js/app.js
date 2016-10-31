41cdc3044811a753242901bcab6ed90dc2c4d597
Peter O'Hara Adu <ohara.invent@gmail.com>
2016-10-31 08:09:18 +0000

    Add signup/login popup

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'firebase'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives', 'starter.services', 'firebase'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -83,4 +83,107 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/current');
 
+});
+
+
+
+angular.module('starter.directives', [])
+.directive('bibble', function($rootScope){
+  // Runs during compile
+  return {
+    // name: '',
+    // priority: 1,
+    // terminal: true,
+    // scope: {}, // {} = isolate, true = child, false/undefined = no change
+    controller: function($rootScope, $scope,  $ionicPopup, $firebaseAuth) {
+      var auth = $firebaseAuth();
+
+
+      $rootScope.auth = auth;
+
+      // console.log("hello", firebaseUser)
+
+
+      // any time auth state changes, add the user data to scope
+      $rootScope.auth.$onAuthStateChanged(function(firebaseUser) {
+        $rootScope.firebaseUser = firebaseUser;
+      });
+
+
+      $scope.showLoginPopup = function () {
+        $rootScope.firebaseUser = null;
+        $scope.error = null;
+        $scope.message = null;
+
+        $scope.data = {};
+
+        var myPopup = $ionicPopup.show({
+            template: '<input type="email" required ng-model="data.email"> <br> <input type="password" required ng-model="data.password">',
+            title: 'Enter Login Details',
+            subTitle: 'Please use your meltwater email',
+            scope: $scope,
+            buttons: [
+              { text: 'Cancel' },
+              {
+                text: '<b>Login</b>',
+                type: 'button-positive',
+                onTap: function(e) {
+                  if (!$scope.data.email) {
+                    console.log("Please enter a valid email");
+                    //don't allow the user to close unless he enters a valid email
+                    e.preventDefault();
+                  }
+                  if (!$scope.data.password) {
+                    console.log("Please enter a valid password");
+                    //don't allow the user to close unless he enters a valid password
+                    e.preventDefault();
+                  } else {
+                    return $scope.data;
+                  }
+                }
+              }
+            ]
+          });
+
+        myPopup.then(function(res) {
+            console.log('Signing in with!', res);
+
+            var auth = $firebaseAuth();
+
+            auth
+            .$signInWithEmailAndPassword(res.email, res.password)
+            .then(function(firebaseUser) {
+              $rootScope.firebaseUser = firebaseUser;
+              console.log("Signed in as:", firebaseUser.uid);
+            })
+            .catch(function(error) {
+              console.error("Authentication failed:", error);
+
+              // Create a new user
+              auth.$createUserWithEmailAndPassword(res.email, res.password)
+                .then(function(firebaseUser) {
+                  $rootScope.firebaseUser = firebaseUser;
+                  $scope.message = "User created with uid: " + firebaseUser.uid;
+                }).catch(function(error) {
+                  $scope.error = error;
+                });
+            });
+
+          });
+      }  
+    },
+    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+    restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+    template: '<ion-nav-buttons side="primary">' +
+    '<button ng-show="firebaseUser === null" class="button" ng-click="showLoginPopup()">Login</button>' +
+    '<button ng-hide="firebaseUser === null" class="button" ng-click="auth.$signOut()">Logout</button>' +
+    '</ion-nav-buttons>',
+    // templateUrl: 'templates/auth-view.html',
+    replace: true,
+    // transclude: true,
+    // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+    link: function($scope, iElm, iAttrs, controller) {
+      console.log("I was here som");
+    }
+  };
 });
